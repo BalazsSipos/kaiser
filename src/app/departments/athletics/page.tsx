552f90e8ecd7a1c8cfbe278f7a0d53891e829120
Balazs Sipos <balazs.sipos.hu@gmail.com>
2024-11-07 14:11:40 +0100

    Add departments

---
+++ src/app/departments/athletics/page.tsx
@@ -0,0 +1,82 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import facebook from '@assets/facebook-white.png';
+import csaszar from '@assets/csaszar_barbara.jpg';
+
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center gap-20 bg-zinc-950 p-5 sm:p-20 py-10">
+      <h1>ATLÉTIKA SZAKOSZTÁLY</h1>
+      <main className="flex flex-col lg:flex-row gap-16 justify-center items-center lg:items-start">
+        <div className="flex flex-col md:flex-row gap-16 basis-2/3 items-center">
+          <div className="flex flex-col basis-1/2 gap-6">
+            <h2 className="self-center">EDZÉSEK</h2>
+            <div className="grid grid-cols-2">
+              <div>Tavasztól őszig:</div>
+              <div>Budapest, XIX. kerület Tichy Lajos Sporttelep</div>
+              <div>télen:</div>
+              <div>Budapest, XIV. kerület BOK csarnok</div>
+            </div>
+            <div className="grid grid-cols-2">
+              <div>Hétfő:</div>
+              <div>18.30-20.00</div>
+              <div>Péntek:</div>
+              <div>18.00-19.30</div>
+              <div>Szombat:</div>
+              <div>10.00-12.00</div>
+            </div>
+          </div>
+          <div className="flex flex-col gap-5 basis-1/2">
+            <div>
+              A szakosztály kiemelten várja a gyermek és felnőtt korosztályon
+              túl a szenior atlétákat, célként tűzve ki, hogy az egyesületen
+              kívül sportoló és versenyző masters atlétákat összefogja,
+              közösséget teremtsen.
+            </div>
+            <div>
+              Amennyiben fiatalon atletizáltál - akár versenyzői, akár
+              szabadidős tevékenység keretében - és kedvet érzel ismét jó
+              hangulatú és az életkori sajátosságokat figyelembe vevő edzéseken
+              részt venni, vagy csak egészségmegőrző mozgást végezni, közöttünk
+              a helyed!
+            </div>
+            <div>
+              A gyermekek jelentkezést már 6 éves kortól várjuk, akikkel játékos
+              foglalkozások keretében ismertetjük meg az atlétika világát,
+              hangsúlyt fektetve a mozgáskoordinációra.
+            </div>
+            <div>
+              Erőnléti edzések keretében szeretettel várjuk a formásodni,
+              erősödni, fogyni vágyókat, személyre szabott edzéstervvel és
+              étrenddel, életkortól, nemtől függetlenül!
+            </div>
+            <div className="flex">
+              <div className="content-center">
+                Keress minket Facebook-on is:
+              </div>
+              <Link
+                className="inline-block px-3"
+                href="https://www.facebook.com/kaisersportegyesulet"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <Image
+                  className="transition duration-500 transform hover:scale-125"
+                  aria-hidden
+                  src={facebook}
+                  alt="File icon"
+                  width={32}
+                />
+              </Link>
+            </div>
+          </div>
+        </div>
+        <div className="basis-1/3">
+          <h2 className="text-center">Edző: Dr. Császár Barbara</h2>
+          <h3 className="text-center">Tel: +36/30-85-999-46</h3>
+          <Image src={csaszar} alt="Kaiser logo" priority />
+        </div>
+      </main>
+    </div>
+  );
+}
---
+++ src/app/departments/fighting/page.tsx
@@ -0,0 +1,95 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import mester from '@assets/mester.jpg';
+import facebook from '@assets/facebook-white.png';
+
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center gap-20 bg-zinc-950 p-5 sm:p-20 py-10">
+      <h1>KÖZELHARC SZAKOSZTÁLY</h1>
+      <main className="flex flex-col lg:flex-row gap-16 justify-center items-center lg:items-start">
+        <div className="flex flex-col md:flex-row gap-16 basis-2/3 items-center">
+          <div className="flex flex-col basis-1/2 gap-6">
+            <h2 className="self-center">EDZÉSEK</h2>
+            <div className="grid grid-cols-2">
+              <div>Helyszín:</div>
+              <div>Budapest, XVII. kerület Uszoda u. 2.</div>
+            </div>
+            <div className="grid grid-cols-2">
+              <div>Kedd:</div>
+              <div>17.00-18.00</div>
+              <div>Péntek:</div>
+              <div>17.00-18.00</div>
+            </div>
+          </div>
+          <div className="flex flex-col gap-5 basis-1/2">
+            <div>
+              Bakos László mester már gyermekkora óta versenyszerűen űzi a
+              küzdősportot, a kung-fu, muaythai, ökölvívás, ju jitsu
+              sporttágokban megmérettetések során van túl, külön kiemelendő,
+              hogy profi ökölvívó múlttal rendelkezik.
+            </div>
+            <div>
+              2018-ban jó minősítéssel ju jitsu edzői végzettséget szerzett.
+            </div>
+            <div>
+              A Magyar Közelharc Kézitusa Szövetségben sikeres vizsgát tett, és
+              3 dan fokozatot ért el ju jitsuban 2020-ban. A honvédelem ügye
+              iránt elkötelezett mesternek - aki a MATASZ tagja is - a katonai
+              életpálya modell népszerűsítése a fő életcélja, amelynek keretében
+              honvéd kadét versenyzőit készíti fel a versenyekre, katonai
+              teljesítménytúrákat teljesít, és nem rest maga sem elindulni
+              különböző megmérettetéseken (pl. atlétikai verseny, NATO futás,
+              Bocskai Portya katonai teljesítménytúra, ect.)
+            </div>
+            <div>
+              Folyamatosan képzi magát, rendszeresen vesz részt különböző
+              küzdősport szemináriumokon, ahová rendszerint tanítványait is
+              elviszi, hogy tapasztalatokat szerezzenek, új technikákat
+              tanuljanak.
+            </div>
+            <div>
+              A tanítványai rendre eredményesen szerepelnek és dobogós
+              helyezéseket hoznak a Magyar Grappling Szövetség által szervezett
+              grappling versenyeken, illetve muaythai, kempo, judo versenyeken.
+              A mester nemcsak a fiatal, hanem a felnőtt és szeniorkorú
+              sportolni vágyókat is várja, hiszen a mai világban az önvédelmi
+              tudás felértékelődött és fontossá vált.
+            </div>
+            <div className="flex">
+              <div className="content-center">
+                Keress minket Facebook-on is:
+              </div>
+              <Link
+                className="inline-block px-3"
+                href="https://www.facebook.com/kaisersportegyesulet"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <Image
+                  className="transition duration-500 transform hover:scale-125"
+                  aria-hidden
+                  src={facebook}
+                  alt="File icon"
+                  width={32}
+                  // height={16}
+                />
+              </Link>
+            </div>
+          </div>
+        </div>
+        <div className="basis-1/3">
+          <h2 className="text-center">Edző: Bakos László 3. dan mester</h2>
+          <h3 className="text-center">Tel: +36/30-49-55-943</h3>
+          <Image
+            src={mester}
+            alt="Kaiser logo"
+            // width={380}
+            // height={38}
+            priority
+          />
+        </div>
+      </main>
+    </div>
+  );
+}
